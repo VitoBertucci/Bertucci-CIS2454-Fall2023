5190ef67008745b6b84aeb5382aa236bb8281add
Vito Bertucci <73321785+VitoBertucci@users.noreply.github.com>
2023-09-17 11:56:11 -0400

    Completed Lab 0
    Class intro discussion post

--- Lab0/index.php
+++ Lab0/index.php
@@ -7,7 +7,40 @@
     </head>
     <body>
         <?php
-        
+        echo 
+        "
+        <h1>1 - Name, and major / reason for taking the class</h1>
+        <h3>
+            My name is Vito Bertucci and I am taking this class because it is a requirement for my
+            program and because I am looking to get into frontend development
+        </h3>
+        <h1>2 - Any previous programming experience that they have</h1>
+        <h3>
+            This is my third year of taking computer science classes and I have taken classes in
+            Java, C#, C++, database management, networking, SQL, data structures, and I am also currently
+            taking an HTML class. Outside of school I have created different projects in Java and Python
+            in order to make my work flow better, and I have completed some bootcamp stuff in Javascript,
+            HTML, and CSS. I have also started to get into different frameworks like React.js.
+        </h3>
+        <h1>3 - Do they have any hobbies?</h1>
+        <h3>
+            I really enjoy golf, I played over 2,000 holes of golf last season,which is roughly 25
+            holes of golf, everyday, for 3 months straight. I also enjoy playing guitar and have been
+            playing for 12 years now, I was even once a teacher with students.
+        </h3>
+        <h1>4 - Do they have any plans for the Fall aside from school?</h1>
+        <h3>
+            This fall I am planning on heading up north at some point with my girlfriend for a camping
+            trip. We are thinking about Pictured Rocks or somewhere in the Upper Peninsula. I also, of
+            course, am planning on playing a bunch of golf, because I love fall golf.
+        </h3>
+        <h1>5 - Anything else they'd like the class to know about them.</h1>
+        <h3>
+            I am excited to learn new things this semester because almost all of my previous classes
+            have been in Java or some sort of C language, and I am not super comfortable with HTML yet
+            so I am excited to get out of that comfort zone.
+        </h3>
+        ";
         ?>
     </body>
 </html>
